dddb2a7cf28fcb13ea17163cbd3f5d8bff48e086
buhaoqi <yeszhangjie@gmail.com>
2021-01-20 00:00:02 -0400

    modified edit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,7 +35,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/buhaoqi/sifangzhan',
+            'https://github.com/buhaoqi/sifangzhan/blob/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
